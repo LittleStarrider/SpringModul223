a08f911ac9655e470e1337389cf379e248940683
little_starrider <fabio.geisser@gmail.com>
2022-09-19 11:41:07 +0200

    added h2db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO PERSON (id, name, password_hash, is_admin)
+VALUES ('1fd2ab72-ed5d-412d-b567-ebd6b643b89d', 'Fabio Geisser', '12345', true);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS PERSON CASCADE;
+CREATE TABLE PERSON
+(
+    id            UUID,
+    name          VARCHAR(2000) NOT NULL,
+    password_hash VARCHAR(2000) NOT NULL,
+    is_admin      BOOLEAN       NOT NULL DEFAULT FALSE,
+
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS BOOKING CASCADE;
+CREATE TABLE BOOKING
+(
+    id       UUID,
+    checkin  DATE,
+    checkout DATE,
+    amount   int,
+    approved BOOL,
+    fk_person UUID,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (fk_person) REFERENCES PERSON (id)
+);
